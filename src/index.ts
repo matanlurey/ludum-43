df97d965e76f7cefddb8b884bf9048adfe879f58
Matan Lurey <matanlurey@users.noreply.github.com>
2018-11-28 21:06:49 -0800

    Install phaser, use starter code. (#2)
    * Install phaser, use starter code.
    
    * Format.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
+// tslint:disable:no-magic-numbers
+import * as phaser from 'phaser';
+
+// Global Flags.
 declare const FLAGS_DIMENSIONS: {
   width: number;
   height: number;
 };
-declare const FLAGS_PRODUCTION: boolean;
-declare const FLAGS_VERSION: string;
 
-const div = document.querySelector('div')!;
-div.style.width = `${FLAGS_DIMENSIONS.width}px`;
-div.style.height = `${FLAGS_DIMENSIONS.height}px`;
-div.textContent = 'Hello World';
+// Test Scene
+class HelloScene extends phaser.Scene {
+  private player!: phaser.GameObjects.Sprite;
+  private cursors!: phaser.Input.Keyboard.CursorKeys;
+
+  constructor() {
+    super({ key: 'HelloScene' });
+  }
+
+  public preload(): void {
+    this.load.tilemapTiledJSON('map', 'src/assets/desert.json');
+    this.load.image('desert', 'src/assets/desert.png');
+    this.load.image('player', 'src/assets/mushroom.png');
+  }
+
+  public create(): void {
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('desert');
+    map.createStaticLayer(0, tileset, 0, 0);
+
+    this.player = this.add.sprite(100, 100, 'player');
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player, false);
+  }
+
+  public update(_: number, __: number): void {
+    this.player.angle += 1;
+    if (this.cursors.left!.isDown) {
+      this.player.x -= 5;
+    }
+    if (this.cursors.right!.isDown) {
+      this.player.x += 5;
+    }
+    if (this.cursors.down!.isDown) {
+      this.player.y += 5;
+    }
+    if (this.cursors.up!.isDown) {
+      this.player.y -= 5;
+    }
+  }
+}
+
+(() => {
+  // Constructor has side-effects.
+  // tslint:disable-next-line:no-unused-expression
+  new phaser.Game({
+    type: phaser.AUTO,
+    // tslint:disable-next-line:object-literal-sort-keys
+    parent: 'content',
+    width: FLAGS_DIMENSIONS.width,
+    height: FLAGS_DIMENSIONS.height,
+    resolution: 1,
+    backgroundColor: '#EDEEC9',
+    scene: [HelloScene],
+  });
+})();
